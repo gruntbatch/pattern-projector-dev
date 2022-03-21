0bb9feeb464bc36af064ff678ca895d8aa4a399c
gruntbatch <gruntbatch@gmail.com>
2022-03-21 15:10:13 -0700

    Add shaders for handles

--- js/index.ts
+++ js/index.ts
@@ -4,6 +4,34 @@
 // select handles
 // move handles
 
+const handleVertexShaderSource = `#version 300 es
+precision mediump float;
+
+layout (std140) uniform Matrices {
+    mat4 projection;
+    mat4 view;
+    mat4 model;
+};
+
+layout (location=0) in vec2 in_position;
+
+void main(void) {
+    gl_Position = projection * view * model * vec4(in_position, 1.0, 1.0);
+}
+`;
+
+const handleFragmentShaderSource = `#version 300 es
+precision mediump float;
+
+uniform vec4 in_color;
+
+out vec4 out_color;
+
+void main(void) {
+    out_color = in_color;
+}
+`;
+
 const vertexShaderSource = `#version 300 es
 
 precision mediump float;
