f2f6a4ce5c906694fa3fe6dba5149e6b08fddb18
gruntbatch <gruntbatch@gmail.com>
2022-05-26 07:13:49 -0700

    Hide and show the menu

--- src/interface.ts
+++ src/interface.ts
@@ -1,7 +1,18 @@
+enum MenuMode {
+    Calibration,
+    Hidden,
+    Pattern
+}
+
 class Interface {
     menu: HTMLElement;
+    menuMode: MenuMode;
+    previousMenuMode: MenuMode;
+    menuHide: HTMLElement;
     menuLeft: boolean;
     menuSwap: HTMLElement;
+
+    calibrationPanel: HTMLElement;
     zoomReset: HTMLElement;
     onZoomReset: () => void;
     zoomValue: HTMLElement;
@@ -11,15 +22,35 @@ class Interface {
     handleXValues: Array<HTMLElement>;
     handleYValues: Array<HTMLElement>;
 
+    patternPanel: HTMLElement;
+
+    saveLoadPanel: HTMLElement;
+
     constructor () {
         this.menu = document.getElementById("menu");
+        this.menuMode = MenuMode.Calibration;
+        this.previousMenuMode = this.menuMode;
+        this.menuHide = document.getElementById("hide");
+        this.menuHide.onclick = () => {
+            if (this.menuMode == MenuMode.Hidden) {
+                this.menuHide.innerHTML = "Hide";
+                this.menuMode = this.previousMenuMode;
+                this.updateMenuMode();
+            } else {
+                this.menuHide.innerHTML = "Show";
+                this.previousMenuMode = this.menuMode;
+                this.menuMode = MenuMode.Hidden
+                this.updateMenuMode();
+            }
+        }
         this.menuLeft = true;
-        this.swapMenu();
         this.menuSwap = document.getElementById("swap");
         this.menuSwap.onclick = () => {
             this.menuLeft = !this.menuLeft;
             this.swapMenu();
         };
+
+        this.calibrationPanel = document.getElementById("calibration-panel");
         this.zoomReset = document.getElementById("zoom-reset");
         this.zoomReset.onclick = () => {
             this.onZoomReset();
@@ -37,6 +68,35 @@ class Interface {
             }
             this.handleXValues[i] = document.getElementById("x-" + i);
             this.handleYValues[i] = document.getElementById("y-" + i);
+        }
+
+        this.patternPanel = document.getElementById("pattern-panel");
+
+        this.saveLoadPanel = document.getElementById("save-load-panel");
+
+        this.swapMenu();
+        this.updateMenuMode();
+    }
+
+    updateMenuMode() {
+        switch(this.menuMode) {
+            case MenuMode.Calibration:
+                this.calibrationPanel.classList.remove("display-none");
+                this.patternPanel.classList.add("display-none");
+                this.saveLoadPanel.classList.remove("display-none");
+                break;
+
+            case MenuMode.Hidden:
+                this.calibrationPanel.classList.add("display-none");
+                this.patternPanel.classList.add("display-none");
+                this.saveLoadPanel.classList.add("display-none");
+                break;
+
+            case MenuMode.Pattern:
+                this.calibrationPanel.classList.add("display-none");
+                this.patternPanel.classList.remove("display-none");
+                this.saveLoadPanel.classList.remove("display-none");
+                break;
         }
     }
 
