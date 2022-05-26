e4822ff44f686f731973b5342ce0af1e36941f01
gruntbatch <gruntbatch@gmail.com>
2022-05-25 20:27:29 -0700

    Improve the build system ... yet again

--- watch-and-build.py
+++ watch-and-build.py
@@ -1,94 +1,100 @@
+import glob
+import itertools
 import os
 import subprocess
 import time
 
 
-def _build(infile, dependencies, lines):
-    dependencies.add(infile)
-
-    with open(infile, "r") as f:
-        for line in f.readlines():
-            if line.startswith("//#include"):
-                filename = line.split("(\"", 1)[1].split("\")", 1)[0]
-                _build(filename, dependencies, lines)
-            
-            elif (index := line.find("#include")) != -1:
-                prefix = line[:index]
-                filename, suffix = line[index:].split("(\"", 1)[1].split("\")", 1)
-
-                lines.append(prefix)
-                _build(filename, dependencies, lines)
-                lines.append(suffix)
-
-            else:
-                lines.append(line)
+def scan_for_source_files(sourcedirs: set[str]):
+    sources = set()
+    for sourcedir in sourcedirs:
+        sources |= set(glob.glob(os.path.join(sourcedir, "*.ts")))
+    return sources
 
 
-
-def build(infile, outfile):
-    dependencies = set()
-    lines = []
-    _build(infile, dependencies, lines)
-
-    with open(outfile, "w") as f:
-        for line in lines:
-            f.write(line)
-
-    return dependencies
-
-
-def watch(dependencies):
-    dts = {d: os.stat(d).st_mtime for d in dependencies}
+def watch_for_source_files(sourcedirs, dependencies: set[str]):
+    sources = scan_for_source_files(sourcedirs)
+    files_of_interest = sources | dependencies
+    foi2ts = {foi: os.stat(foi).st_mtime for foi in files_of_interest}
 
     while True:
         time.sleep(1)
 
-        # TODO Handle file deletion
-        for d, ts in dts.items():
-            if os.stat(d).st_mtime != ts:
-                return
-                    
+        sources2 = scan_for_source_files(sourcedirs)
+        if sources != sources2:
+            return sources2
+
+        for foi, ts in foi2ts.items():
+            try:
+                if os.stat(foi).st_mtime != ts:
+                    return sources2
+            except FileNotFoundError:
+                return sources2
 
 
-def watch_and_build(builddir, outfile, infiles):
-    while True:
-        # Build
-        dependencies = set()
-        tsc_infiles = set()
-        for infile in infiles:
-            _outfile = os.path.join(builddir, os.path.basename(infile))
-            tsc_infiles.add(_outfile)
 
-            infile_dependencies = build(infile, _outfile)
+def preprocess_and_compile(sources, builddir, outfile):
+    dependencies = set()
+    processed_sources = set()
+    preprocess(sources, builddir, dependencies, processed_sources)
+    compile(processed_sources, outfile)
+    return dependencies
 
-            for infile_dependency in infile_dependencies:
-                dependencies.add(infile_dependency)
 
-        cmd = [
-            "tsc", "--outFile", outfile, *tsc_infiles
-        ]
+def preprocess(sources, builddir, out_dependencies, out_sources):
+    for source in sources:
+        lines = []
+        _preprocess(source, out_dependencies, lines)
 
-        # Compile
-        print()
-        print("Building ...")
-        print()
-        print(*cmd)
-        result = subprocess.run(
-            cmd,
-            stdout=subprocess.PIPE
-        )
-        print(result.stdout.decode("utf-8"))
+        out_source = os.path.join(builddir, os.path.basename(source))
+        with open(out_source, "w") as f:
+            for line in lines:
+                f.write(line)
 
-        if result.returncode != 0:
-            print("... Done! Errors detected!")
+        out_sources.add(out_source)
 
-        else:
-            print("... Done! Watching these files for changes:")
-            for d in dependencies:
-                print("\t", d, sep="")
 
-        # Watch        
-        watch(dependencies)
+def _preprocess(source, out_dependencies, out_lines):
+    out_dependencies.add(source)
+
+    with open(source, "r") as f:
+        for line in f.readlines():
+            if line.startswith("//#include") or line.startswith("// #include"):
+                source2 = line.split("(\"", 1)[1].split("\")", 1)[0]
+                _preprocess(source2, out_dependencies, out_lines)
+
+            elif (index := line.find("#include")) != -1:
+                prefix = line[:index]
+                source2, suffix = line[index:].split("(\"", 1)[1].split("\")", 1)
+
+                out_lines.append(prefix)
+                _preprocess(source2, out_dependencies, out_lines)
+                out_lines.append(suffix)
+
+            else:
+                out_lines.append(line)
+
+
+def compile(sources, outfile):
+    cmd = [
+        "tsc", "--outFile", outfile, *sources
+    ]
+
+    print()
+    print("Compiling ...")
+    print()
+    print(*cmd)
+    result = subprocess.run(
+        cmd,
+        stdout=subprocess.PIPE
+    )
+    print(result.stdout.decode("utf-8"))
+
+    if result.returncode != 0:
+        print("... Done! Errors detected!")
+
+    else:
+        print("... Done!")
 
 
 if __name__ == "__main__":
@@ -96,11 +102,18 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--builddir", type=str)
     parser.add_argument("--outfile", type=str)
-    parser.add_argument("infiles", nargs="+")
+    parser.add_argument("--sourcedir", type=str, dest="sourcedirs", action="append")
     args = parser.parse_args()
 
-    builddir = args.builddir
-    outfile = args.outfile
-    infiles = args.infiles
+    # scan sourcedirs for additional source files
+    sources = scan_for_source_files(set(args.sourcedirs))
 
-    watch_and_build(builddir, outfile, infiles)
+    # preprocess and compile files
+    dependencies = preprocess_and_compile(sources, args.builddir, args.outfile)
+
+    while True:
+        # watch for new or changed sources
+        sources = watch_for_source_files(args.sourcedirs, dependencies)
+
+        # preprocess and compile files
+        dependencies = preprocess_and_compile(sources, args.builddir, args.outfile)
