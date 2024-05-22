c892b77328a6ede0cc313897266f3559335e506b
gruntbatch <gruntbatch@gmail.com>
2024-05-21 17:44:51 -0700

    Draw a triangle

--- ts/index.ts
+++ ts/index.ts
@@ -1,6 +1,29 @@
-import { foobar } from "./render.js";
+import * as render from "./render.js";
 
 (() => {
-    console.log("Hello, world!");
-    foobar();
+    const glCanvas = document.getElementById("gl-canvas") as HTMLCanvasElement | null;
+    if (!glCanvas) {
+        throw new Error();
+    }
+
+    const glContext = glCanvas.getContext("webgl");
+    if (!glContext) {
+        throw new Error();
+    }
+
+    render.setContext(glContext);
+
+    const buffer = new render.Buffer();
+    const mTriangle = buffer.mesh([
+        render.newVertex([-1, -1]),
+        render.newVertex([0, 1]),
+        render.newVertex([1, -1])
+    ]);
+    const pHello = new render.Program("glsl/hello.vert", "glsl/hello.frag");
+
+    const onAnimationFrame = () => {
+        mTriangle.draw(pHello);
+        requestAnimationFrame(onAnimationFrame);
+    }
+    onAnimationFrame();
 })();
--- ts/render.ts
+++ ts/render.ts
@@ -1,7 +1,148 @@
 export {
-    foobar
+    newVertex,
+    setContext,
+
+    Buffer,
+    Program,
 };
 
-function foobar() {
-    console.log("Hello, module!");
+type Point = [number, number];
+type Color = [number, number, number, number];
+type Vertex = [number, number, number, number, number, number, number, number];
+
+const VERTEX_ELEMENT_SIZE = 4;
+const VERTEX_ELEMENT_COUNT = 8;
+
+let gl: WebGLRenderingContext;
+let currentBuffer: WebGLBuffer | null = null;
+let currentProgram: WebGLProgram | null = null;
+
+function newVertex(position: Point = [0, 0], texCoord: Point = [0, 0], color: Color = [1, 0, 0, 1]): Vertex {
+    return [
+        position[0], position[1],
+        texCoord[0], texCoord[1],
+        color[0], color[1], color[2], color[3]
+    ];
+}
+
+// Of course typescript is a fucking idiot and asks me to write a getter instead
+// of expose a variable. I shouldn't be surprised.
+function setContext(glContext: WebGLRenderingContext) {
+    gl = glContext;
+}
+
+class Buffer {
+    capacity: number;
+    length: number = 0;
+    buffer: WebGLBuffer;
+
+    constructor(capacity = 512) {
+        this.capacity = capacity;
+
+        this.buffer = gl.createBuffer();
+        this.bind();
+        gl.bufferData(
+            gl.ARRAY_BUFFER,
+            VERTEX_ELEMENT_SIZE * VERTEX_ELEMENT_COUNT * this.capacity,
+            gl.DYNAMIC_DRAW
+        );
+
+        // position
+        gl.vertexAttribPointer(0, 2, gl.FLOAT, false, 32, 0);
+        gl.enableVertexAttribArray(0);
+
+        // texCoord
+        gl.vertexAttribPointer(1, 2, gl.FLOAT, false, 32, 8);
+        gl.enableVertexAttribArray(1);
+
+        // color
+        gl.vertexAttribPointer(2, 4, gl.FLOAT, false, 32, 16);
+        gl.enableVertexAttribArray(2);
+    }
+
+    bind() {
+        if (currentBuffer != this.buffer) {
+            currentBuffer = this.buffer;
+            gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
+        }
+
+        return true;
+    }
+
+    mesh(vertices: Array<Vertex>, mode: number = gl.TRIANGLES): Mesh {
+        if (this.length + vertices.length > this.capacity) {
+            throw new Error("Too many vertices!");
+        }
+
+        let data = new Float32Array(vertices.length * VERTEX_ELEMENT_COUNT);
+        for (let i = 0; i < vertices.length; i++) {
+            const vertex = vertices[i];
+            const index = i * VERTEX_ELEMENT_COUNT;
+            for (let j = 0; j < VERTEX_ELEMENT_COUNT; j++) {
+                data[index + j] = vertex[j];
+            }
+        }
+
+        if (!this.bind()) {
+            throw new Error("Unable to bind buffer for writing!");
+        }
+        gl.bufferSubData(gl.ARRAY_BUFFER, VERTEX_ELEMENT_SIZE * VERTEX_ELEMENT_COUNT * this.length, data);
+
+        let mesh = new Mesh(this, mode, this.length, vertices.length);
+        this.length += vertices.length;
+        return mesh;
+    }
+}
+
+class Mesh {
+    constructor(public buffer: Buffer, public mode: number, public first: number, public count: number) {
+
+    }
+
+    draw(program: Program) {
+        if (program.bind() && this.buffer.bind()) {
+            gl.drawArrays(this.mode, this.first, this.count);
+        }
+    }
+}
+
+class Program {
+    program: WebGLProgram | null;
+
+    constructor(vertUrl: string, fragUrl: string) {
+        this.program = null;
+        this.load(vertUrl, fragUrl);
+    }
+
+    private async load(vertUrl: string, fragUrl: string) {
+        let response = await fetch(vertUrl);
+        let content = await response.text();
+        let vert = gl.createShader(gl.VERTEX_SHADER);
+        gl.shaderSource(vert, content);
+        gl.compileShader(vert);
+
+        response = await fetch(fragUrl);
+        content = await response.text();
+        let frag = gl.createShader(gl.FRAGMENT_SHADER);
+        gl.shaderSource(frag, content);
+        gl.compileShader(frag);
+
+        this.program = gl.createProgram();
+        gl.attachShader(this.program, vert);
+        gl.attachShader(this.program, frag);
+        gl.linkProgram(this.program);
+    }
+
+    bind() {
+        if (!this.program) {
+            return false;
+        }
+
+        if (currentProgram != this.program) {
+            currentProgram = this.program;
+            gl.useProgram(this.program);
+        }
+
+        return true;
+    }
 }
